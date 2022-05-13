4608c453eee9a404ed2acd00629f38e43e1da21d
Ninnias Bieler <>
2022-05-13 11:55:19 +0200

    Add buttons formatted as bulletpoints

--- helper-function/whatsapp-send-message.js
+++ helper-function/whatsapp-send-message.js
@@ -22,7 +22,22 @@ const sendMessage = async (answer, senderID) => {
         console.log(`Error at sendMessage --> ${error}`);
       }
     } else if (messageType == "button") {
-
+      let adress = "Du kannst folgende Stichworte antworten, um mehr zu erfahren: ";
+      let bullets = [];
+      let buttonAmount = answer.messages[i].content[0].buttons.length;
+      for (let j = 0; j < buttonAmount; j++) {
+        console.log(answer.messages[i].content[0].buttons[j].content[0].text);
+        bullets.push("\n" + (j+1) + '. "*' + answer.messages[i].content[0].buttons[j].content[0].text + '*"');
+      }
+      try {
+        await client.messages.create({
+          to: senderID,
+          body: adress + bullets.join(" "),
+          from: `whatsapp:+14155238886`,
+        });
+      } catch (error) {
+        console.log(`Error at sendMessage --> ${error}`);
+      }
     } else if (messageType == "link" || messageType == "youtube") {
       try {
         await client.messages.create({
@@ -34,19 +49,22 @@ const sendMessage = async (answer, senderID) => {
         console.log(`Error at sendMessage --> ${error}`);
       }
     } else if (messageType == "slider") {
-      for (let j = 0; j < answer.messages[i].content[0].slides.length; j++){
+      for (let j = 0; j < answer.messages[i].content[0].slides.length; j++) {
         try {
           await client.messages.create({
             to: senderID,
             mediaUrl: answer.messages[i].content[0].slides[j].image.url,
-            body: answer.messages[i].content[0].slides[j].image.name + "\n \n" + answer.messages[i].content[0].slides[j].image.url,
+            body:
+              answer.messages[i].content[0].slides[j].image.name +
+              "\n \n" +
+              answer.messages[i].content[0].slides[j].image.url,
             from: `whatsapp:+14155238886`,
           });
         } catch (error) {
           console.log(`Error at sendMessage --> ${error}`);
         }
       }
-    } else if (messageType == "video" || messageType == "audio" ||  messageType == "image") {
+    } else if (messageType == "video" || messageType == "audio" || messageType == "image") {
       try {
         await client.messages.create({
           to: senderID,
