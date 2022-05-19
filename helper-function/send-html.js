b6a3dcc51fbd841fdcde3c9adca2448686499e75
Renayd <rene.voss@student.fh-kiel.de>
2022-05-19 15:07:55 +0200

    Manueller Merge mit html-strip

--- helper-function/send-html.js
+++ helper-function/send-html.js
@@ -5,13 +5,40 @@ const client = require("twilio")(accountSid, authToken, {
   lazyLoading: true,
 });
 
-const sendMessage = async (answer, senderID, i) => {
+const sendMessage = async (answer, senderID, i) => {     
+    //strip html
+    let string = answer.messages[i].content[0].text;
+    let strippedstring = string.replace(/<[^>]*>?/gm, '');
+
+    //extract link from message and create a new message with the link
+    let urlRE= new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?([^ ])+");
+    let linkRaw= string.match(urlRE);
+    let link= linkRaw.slice(0,1)
+    console.log("raw:"+ linkRaw)
+    console.log("link:"+ link)
+    
   try {
     await client.messages.create({
       to: senderID,
-      body: answer.messages[i].content[0].text,
+      body: strippedstring,
       from: `whatsapp:+14155238886`,
     });
+
+    if (link != null) {         
+    try {
+      await client.messages.create({
+        to: senderID,
+        body: link,
+        from: `whatsapp:+14155238886`,
+      });
+
+      //catch for inner try statement
+    } catch (error) {
+      console.log(`Error at sendMessage --> ${error}`);
+    }
+    };
+
+    //catch for outer try statement
   } catch (error) {
     console.log(`Error at sendMessage --> ${error}`);
   }
